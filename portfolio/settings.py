13016e4244bd802022db2c01a40298d4a05ec38a
chrisjen83 <chrisjen83@gmail.com>
2021-12-14 10:46:41 +1100

    Update to work in container

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
 }
 
 
